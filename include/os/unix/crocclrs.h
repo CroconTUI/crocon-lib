294b69346041a43dce366ad428c77c0e335873cd
Dmitry Tretyakov <tinelix@mail.ru>
2026-04-24 12:09:34 +0700

    Add source files for UNIX/Linux

---
+++ include/os/unix/crocclrs.h
@@ -0,0 +1,9 @@
+#ifndef _CROCON_OS_WIN32_CROCCLRS_H
+#define _CROCON_OS_WIN32_CROCCLRS_H
+
+#include <os/unix/crocunix.h>
+#include <utils/colors.h>
+
+extern const char* _crocon_pickcolor(rgbi4_t bg_color, rgbi4_t fg_color);
+
+#endif
---
+++ include/os/unix/crocunix.h
@@ -0,0 +1,8 @@
+#ifndef _CROCON_OS_WIN32_CROCW32_H
+#define _CROCON_OS_WIN32_CROCW32_H
+
+#include <utils/c_exts.h>
+
+extern FILE* _crocon_stdout;
+
+#endif
